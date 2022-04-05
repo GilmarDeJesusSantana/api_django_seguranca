a92c907d4494d6065fd2e6e0f4ac949ac0af9e34
Gilmar <gilmar.jesus@gmail.com>
2022-04-05 16:50:44 -0300

    Url de admin auterada com objetivo de melhorar a segurança.

--- setup/settings.py
+++ setup/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#p@o#!g)%qex2r6@pyq1+w#f7al)=&n($le7gwngc$k&ieo(lm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
